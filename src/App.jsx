43cf083414881257967688b83cd70fed47bd232f
Doni <donibro357@gmail.com>
2024-04-22 20:56:19 +0600

    add swiper for mainpage

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import './App.css'
-import Header from './components/Header/Header'
+import Header from './components/Header/Header';
+import MainPage from './pages/MainPage/MainPage';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
 
   return (
     <>
     <Header/>
+
+    <Routes>
+      <Route path='/' element={<MainPage/>}/>
+    </Routes>
     </>
   )
 }
