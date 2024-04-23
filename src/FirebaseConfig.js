8dfa59fd2eb8dd7038e9a39222c7c5772cff577c
Doni <donibro357@gmail.com>
2024-04-23 21:54:20 +0600

    add firebase config

---
+++ src/FirebaseConfig.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB2R8bFsSf-SXhNd2DvMaiHxaoBmDgk_JM",
+  authDomain: "webshop-9ef92.firebaseapp.com",
+  projectId: "webshop-9ef92",
+  storageBucket: "webshop-9ef92.appspot.com",
+  messagingSenderId: "670230415263",
+  appId: "1:670230415263:web:07f4a585725937f4965faa",
+  measurementId: "G-SE1X95DX0Q"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
