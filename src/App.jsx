6e74203f22245d57f5e54b6c6bafd52f1dd409ad
Benzema9Real <footballfceasports>
2024-04-27 11:55:04 +0600

    add auth

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Header from './components/Header/Header';
 import MainPage from './pages/MainPage/MainPage';
 import { Routes, Route } from 'react-router-dom';
+import SingIn from './components/Header/authentification/singIn';
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
     <div className='mt-20'>
     <Routes>
       <Route path='/' element={<MainPage/>}/>
+      <Route path='/profile' element={<SingIn/>}/>
     </Routes>
     </div>
     </>
---
+++ src/components/Header/authentification/singIn.jsx
@@ -0,0 +1,16 @@
+function SingIn(){
+    return(
+        <div className="w-[100] flex items-center justify-center">
+            <form className="w-[60] flex flex-wrap flex-col">
+            <h1 className="text-[100] font-[500] ">
+                    Sign In Page
+                </h1>
+                <div className="border-">
+                    <img src="./src/assets/Google.png" alt="" ></img>
+                    <h2>Continue With Google</h2>
+                </div>
+           </form>
+        </div>
+    )
+}
+export default SingIn
