8fb6d16089df9c376b5196c1a383753db4db28a5
asiyatynymbekova <asiyatynymbekova@gmail.com>
2024-04-27 11:58:43 +0600

    add auth

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,18 @@ import './App.css'
 import Header from './components/Header/Header';
 import MainPage from './pages/MainPage/MainPage';
 import { Routes, Route } from 'react-router-dom';
+import Footer from './components/Footer'
 
 function App() {
 
   return (
     <>
     <Header/>
-
+  
     <Routes>
       <Route path='/' element={<MainPage/>}/>
     </Routes>
+    <Footer/>
     </>
   )
 }
