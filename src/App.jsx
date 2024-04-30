18d977c274e5b986d05a7f78e348be01ec905293
Benzema9Real <footballfceasports>
2024-04-30 17:32:02 +0600

    hide auth

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import './App.css'
 import Header from './components/Header/Header';
 import MainPage from './pages/MainPage/MainPage';
 import { Routes, Route } from 'react-router-dom';
-import SingIn from './components/Header/authentification/singIn';
-import SingUp from './components/Header/authentification/singup';
+// import SingIn from './components/authentification/singIn';
+// import SingUp from './components/authentification/singup';
 
 function App() {
 
@@ -14,8 +14,8 @@ function App() {
     <div className='mt-20'>
     <Routes>
       <Route path='/' element={<MainPage/>}/>
-      <Route path='/profile' element={<SingIn/>}/>
-      <Route path='/SingUp' element={<SingUp/>}/>
+      {/* <Route path='/profile' element={<SingIn/>}/> */}
+      {/* <Route path='/SingUp' element={<SingUp/>}/> */}
     </Routes>
     </div>
     </>
