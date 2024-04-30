e53ace2b948cfe70d8fe9bf70548ccd3da99b7a5
Benzema9Real <footballfceasports>
2024-04-30 17:52:58 +0600

    add

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Header from './components/Header/Header';
 import MainPage from './pages/MainPage/MainPage';
 import Basket from './pages/BasketPage/Basket';
 import { Routes, Route } from 'react-router-dom';
-import Footer from './components/footer';
+import Footer from './components/Footer';
 // import SingIn from './components/authentification/singIn';
 // import SingUp from './components/authentification/singup';
 
