c7e743955d002f3d4fb2eebf37176b5197b9bafa
Doni <donibro357@gmail.com>
2024-04-30 16:03:07 +0600

    add profile page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Header from './components/Header/Header';
 import MainPage from './pages/MainPage/MainPage';
 import Basket from './pages/BasketPage/Basket';
+import Profile from './pages/ProfilePage/Profile';
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -11,10 +12,11 @@ function App() {
     <Header/>
 
     <div className='mt-[55px]'>
-    <Routes>
-      <Route path='/' element={<MainPage/>}/>
-      <Route path='/basket' element={<Basket/>}/>
-    </Routes>
+      <Routes>
+        <Route path='/' element={<MainPage/>}/>
+        <Route path='/basket' element={<Basket/>}/>
+        <Route path='/profile/*' element={<Profile/>}/>
+      </Routes>
     </div>
     </>
   )
---
+++ src/pages/ProfilePage/CustomLink.jsx
@@ -0,0 +1,13 @@
+import { Link, useMatch } from "react-router-dom"
+
+const CustomLink = ({to, text, image}) => {
+    const match = useMatch(to);
+    return <Link to={to} style={{backgroundColor: match ? '#F6F6F6' : 'white', transition: '0.5s', fontSize: '17px'}}>
+        <div className="flex gap-x-3">
+            <img src={image} alt="" />
+            <p>{text}</p>
+        </div>
+    </Link>
+}
+
+export default CustomLink;
