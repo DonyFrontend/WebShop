b917a82e2528fb52d1799be8092fa2509b486c5a
Doni <donibro357@gmail.com>
2024-04-30 22:42:18 +0600

    add wishlist

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Header from './components/Header/Header';
 import MainPage from './pages/MainPage/MainPage';
 import Basket from './pages/BasketPage/Basket';
 import Profile from './pages/ProfilePage/Profile';
-import WishList from './pages/ProfilePage/ProfilePages/WishList';
 import { Routes, Route } from 'react-router-dom';
 import Footer from './components/Footer';
 // import SingIn from './components/authentification/singIn';
@@ -20,7 +19,6 @@ function App() {
         <Route path='/' element={<MainPage/>}/>
         <Route path='/basket' element={<Basket/>}/>
         <Route path='/profile/*' element={<Profile/>}/>
-        <Route path='/profile/wishlist' element={<WishList/>}/>
       </Routes>
     </div>
 
