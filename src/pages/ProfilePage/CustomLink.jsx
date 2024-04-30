ed6c528f3168c5c93e6fac1d71a04a6de60795b4
Doni <donibro357@gmail.com>
2024-04-30 17:56:37 +0600

    change customLink

--- src/pages/ProfilePage/CustomLink.jsx
+++ src/pages/ProfilePage/CustomLink.jsx
@@ -4,7 +4,7 @@ const CustomLink = ({to, text, image}) => {
     const match = useMatch(to);
     return <Link to={to} style={{backgroundColor: match ? '#F6F6F6' : 'white', transition: '0.5s', fontSize: '17px'}}>
         <div className="flex gap-x-3">
-            <img src={image} alt="" />
+            <img src={image} alt="Error!" />
             <p>{text}</p>
         </div>
     </Link>
