9258b2bce35fd5bcc3f04e42322ca8d07e4472f9
Doni <donibro357@gmail.com>
2024-05-02 18:03:58 +0600

    test

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,18 @@ import Basket from './pages/BasketPage/Basket';
 import Profile from './pages/ProfilePage/Profile';
 import WishList from './pages/ProfilePage/ProfilePages/WishList';
 import { Routes, Route } from 'react-router-dom';
+import {useDispatch, useSelector} from 'react-redux'
+import { shopTC } from './Slices/getProductsSlice';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    dispatch(shopTC());
+  }, [])
+
+  const selector = useSelector(state => state.getProductsSlice);
+  console.log(selector);
+  const dispatch = useDispatch();
 
   return (
     <>
@@ -20,6 +30,17 @@ function App() {
         <Route path='/profile/wishlist' element={<WishList/>}/>
       </Routes>
     </div>
+
+    {selector.products.map((item, index) => <div key={index}>
+      <div>categories {item.categories}</div>
+      <div>colors {item.colors}</div>
+      <div>desc {item.description}</div>
+      <div>
+        {item.images.map((item, index) => <div key={index}>
+          <img style={{width: 200}} src={item} alt="" />
+        </div>)}
+      </div>
+    </div>)}
     </>
   )
 }
--- src/pages/ProfilePage/Profile.jsx
+++ src/pages/ProfilePage/Profile.jsx
@@ -6,10 +6,47 @@ import CustomLink from './CustomLink';
 import { Routes, Route } from 'react-router-dom';
 import UserPage from './ProfilePages/User';
 import WishList from './ProfilePages/WishList';
+import { auth } from '../../FirebaseConfig';
+import { onAuthStateChanged } from 'firebase/auth';
+import { useState } from 'react';
 
 const Profile = () => {
+    const [user, setUser] = useState(null);
+      onAuthStateChanged(auth, user => {
+    user ? setUser(true) : setUser(false);
+  })
 
-    return <div className="flex w-[100%] justify-center mt-32">
+    // return <div className="flex w-[100%] justify-center mt-32">
+    //     <div className="w-[90%] flex justify-between">
+    //         <div className='w-[20%] flex flex-col gap-y-12 p-5' style={{borderRight: '1px solid black'}}>
+    //             <div className='flex flex-col gap-y-2'>
+    //                 <div className='flex gap-x-2'>
+    //                     <span style={{width: 5, height: 40, backgroundColor: '#8A33FD', borderRadius: 15}}></span>
+    //                     <h1 className='font-semibold text-3xl'>Hello Arman</h1>
+    //                 </div>
+    //                 <p className='text-gray-400'>Welcome to your Account</p>
+    //             </div>
+
+    //             <div className='flex flex-col gap-y-4'>
+    //                 <CustomLink image={Orders} text={'My orders'} to={'/profile/orders'}/>   
+    //                 <CustomLink image={Heart} text={'Wishlist'} to={'/profile/wishlist'}/>   
+    //                 <CustomLink image={User} text={'My info'} to={'/profile/user'}/>   
+    //                 <CustomLink image={SignOut} text={'Sign out'} to={'/profile/signout'}/>   
+    //             </div>
+    //         </div>
+
+    //         <div className='w-[75%]'>
+    //             <Routes>
+    //                 <Route path='/*' element={<UserPage/>}></Route>
+    //                 <Route path='wishlist' element={<WishList/>}></Route>
+    //             </Routes>
+    //         </div>
+    //     </div>
+    // </div>
+
+    return (
+        <>
+        {user ? <div className="flex w-[100%] justify-center mt-32">
         <div className="w-[90%] flex justify-between">
             <div className='w-[20%] flex flex-col gap-y-12 p-5' style={{borderRight: '1px solid black'}}>
                 <div className='flex flex-col gap-y-2'>
@@ -35,7 +72,9 @@ const Profile = () => {
                 </Routes>
             </div>
         </div>
-    </div>
+    </div> : <h1>You don`t have account. Please sign in or sign up</h1>}
+        </>
+    )
 }
 
 export default Profile;
