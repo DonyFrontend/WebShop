3f9016c6cd0604fb6938f0524a12809674c6921e
Doni <donibro357@gmail.com>
2024-05-03 12:27:29 +0600

    change

--- src/pages/ProfilePage/Profile.jsx
+++ src/pages/ProfilePage/Profile.jsx
@@ -16,34 +16,6 @@ const Profile = () => {
     user ? setUser(true) : setUser(false);
   })
 
-    // return <div className="flex w-[100%] justify-center mt-32">
-    //     <div className="w-[90%] flex justify-between">
-    //         <div className='w-[20%] flex flex-col gap-y-12 p-5' style={{borderRight: '1px solid black'}}>
-    //             <div className='flex flex-col gap-y-2'>
-    //                 <div className='flex gap-x-2'>
-    //                     <span style={{width: 5, height: 40, backgroundColor: '#8A33FD', borderRadius: 15}}></span>
-    //                     <h1 className='font-semibold text-3xl'>Hello Arman</h1>
-    //                 </div>
-    //                 <p className='text-gray-400'>Welcome to your Account</p>
-    //             </div>
-
-    //             <div className='flex flex-col gap-y-4'>
-    //                 <CustomLink image={Orders} text={'My orders'} to={'/profile/orders'}/>   
-    //                 <CustomLink image={Heart} text={'Wishlist'} to={'/profile/wishlist'}/>   
-    //                 <CustomLink image={User} text={'My info'} to={'/profile/user'}/>   
-    //                 <CustomLink image={SignOut} text={'Sign out'} to={'/profile/signout'}/>   
-    //             </div>
-    //         </div>
-
-    //         <div className='w-[75%]'>
-    //             <Routes>
-    //                 <Route path='/*' element={<UserPage/>}></Route>
-    //                 <Route path='wishlist' element={<WishList/>}></Route>
-    //             </Routes>
-    //         </div>
-    //     </div>
-    // </div>
-
     return (
         <>
         {user ? <div className="flex w-[100%] justify-center mt-32">
