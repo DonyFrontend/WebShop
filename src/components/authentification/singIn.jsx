8ddbdf2c35cf79facedc96aa3d93c7968196f76b
iskak2007 <isaak9894@gmail.com>
2024-05-08 21:11:23 +0600

    add adaptive for auth

--- src/components/authentification/singIn.jsx
+++ src/components/authentification/singIn.jsx
@@ -4,20 +4,20 @@ import twitter from '../../assets/twitter.png'
 function SingIn(){
     return(
         <div className="flex  w-full items-center justify-center">
-            <form className="flex flex-col">
-                <div className="mt-16 my-[6%]">
-                    <h1 className="text-4xl mb-[5%]">
+            <form className="flex items-center lg:items-stretch flex-col">
+                <div className="mt-16 text-center  md:text-left lg:text-left my-[6%]">
+                    <h1 className="mb-10 text-4xl  lg:mb-3">
                         Sign In Page
                     </h1>
                 </div>
-                <div className=" w-[500px] border-[1px] rounded-md border-black">
+                <div className=" w-80 mb-5 lg:mb-0 lg:w-[100%] border-[1px] rounded-md border-black">
                     <div className="flex flex-row p-2 content-evenly justify-center">
                     <img className="mx-[10px] w-5 h-5" src={Google} alt="" ></img>
                         <h2  className="text-[#8A33FD]">Continue With Google</h2>
                     </div>
                 </div>
 
-                <div className="my-[20px] w-[500px] border-[1px] rounded-md border-black">
+                <div className=" mb-5 lg:mb-0 w-80 lg:w-[100%] lg:mt-5 border-[1px] rounded-md border-black">
                     <div className="flex flex-row p-2 content-evenly justify-center">
                     <img className="mx-[10px] w-5 h-5" src={twitter} alt="" ></img>
                         <h2 className="text-[#8A33FD]">Continue With Twitter</h2>
@@ -25,22 +25,32 @@ function SingIn(){
                 </div>
 
                 <div className="flex my-[6%] span-origin items-center justify-between">
-                <div className=" w-[200px] border-y-[1px] border-gray-300">
+                <div className=" lg:w-[200px] border-y-[1px] border-gray-300">
                      </div>
-                        <h1>OR</h1>
-                     <div className="w-[200px]  border-y-[1px] top-none border-gray-300">
+                        <h2 className="hidden lg:flex lg:text-2xl text-gray-600 mx-5">OR</h2>
+                     <div className="lg:w-[200px]  border-y-[1px] top-none border-gray-300">
                      </div>
                 </div>
 
-                <div className=""></div>
+                <div className="text-left">
+                <h2 className=" my-[10px]">User name or email address</h2>
+                </div>
+                <div>
+                    <input required type="email" className="p-2 w-80   lg:w-[100%] border-[1px] rounded-md lg:p-3 border-[#3C4242]"/>
+                </div>
+              
 
-                    <h2 className="my-[10px]">User name or email address</h2>
-                    <input required type="email" className="border-[1px] rounded-md p-3 border-[#3C4242]"/>
+                <div className="text-left">
+                 <h2 className="my-[10px]">Password</h2>
+                </div>
 
-                    <h2 className="my-[10px]">Password</h2>
-                    <input required type="password" className="border-[1px] rounded-md p-3 border-[#3C4242]" />
-                    <Link className="text-end text-[#8A33FD]" to={'https://support.google.com/accounts/answer/41078?hl=en&co=GENIE.Platform%3DDesktop'}>Forget your password?</Link>
-                    <button className="w-[40%] rounded-md border-[1px] border-black p-4 bg-[#8A33FD] text-white"> Sing in</button>
+                <div>
+                    <input required type="password" className="p-2 w-80  lg:w-[100%] border-[1px] rounded-md lg;p-3 border-[#3C4242]" />
+                </div>
+
+
+                    <Link className="my-5 lg:my-0 text-end text-[#8A33FD]" to={'https://support.google.com/accounts/answer/41078?hl=en&co=GENIE.Platform%3DDesktop'}>Forget your password?</Link>
+                    <button className="w-80 lg:w-40 rounded-md border-[1px] border-black p-4 bg-[#8A33FD] text-white"> Sing in</button>
                     <div className="mb-16 flex">
                     <h1>Don’t have an account?</h1><Link to='/singup' className="text-[#8A33FD]">Sign up</Link>  
                     </div>
--- src/components/authentification/singup.jsx
+++ src/components/authentification/singup.jsx
@@ -3,23 +3,23 @@ import Google from '../../assets/Google.png'
 import twitter from '../../assets/twitter.png'
 function SingUp(){
     return(
-        <div className="w-full mb-16 flex items-center justify-center">
-            <form className="flex  flex-col">
-                <div className="my-[10%]">
-                    <h1 className="text-4xl font-medium ">
+        <div className="flex  w-full items-center justify-center">
+            <form className="flex items-center lg:items-stretch flex-col">
+                <div className="mt-16 text-center  md:text-left lg:text-left my-[6%]">
+                    <h1 className=" text-4xl  lg:mb-3">
                         Sign Up
                     </h1>
                 <h2 className="pt-5">Sign up for free to access to in any of our products </h2>
                 </div>
            
-                <div className=" w-[500px] border-[1px] rounded-md border-black">
+                <div className=" w-80 mb-5 lg:mb-0 lg:w-[100%] border-[1px] rounded-md border-black">
                     <div className="flex flex-row p-2 content-evenly justify-center">
                         <img className="mx-3 w-5 h-5" src={Google} alt="" ></img>
                         <h2  className="text-[#8A33FD]">Continue With Google</h2>
                     </div>
                 </div>
 
-                <div className="my-[20px] w-[500px] border-[1px] rounded-md border-black">
+                <div className="mb-5 lg:mb-0 w-80 lg:w-[100%] lg:mt-5 border-[1px] rounded-md border-black">
                     <div className="flex flex-row p-2 content-evenly justify-center">
                         <img className="mx-[10px] w-5 h-5" src={twitter} alt="" ></img>
                         <h2 className="text-[#8A33FD]">Continue With Twitter</h2>
@@ -30,12 +30,12 @@ function SingUp(){
                 <div>
                     <h2 className="my-[3%] mt-[5%]">Email Address</h2>
                 </div>
-                    <input  required  type="email" className="border-[1px] rounded-md p-3 border-[#3C4242]"/>
+                    <input  required  type="email" className="p-2 w-80   lg:w-[100%] border-[1px] rounded-md lg:p-3 border-[#3C4242]"/>
 
                 <div className="my-[2%] mt-[5%]">
                     <h2 className="">Password</h2>
                 </div>
-                    <input  required  type="password" className="border-[1px] rounded-md p-3 border-[#3C4242]" />
+                    <input  required  type="password" className="p-2 w-80  lg:w-[100%] border-[1px] rounded-md lg;p-3 border-[#3C4242]" />
                     <h2>Use 8 or more characters with a mix of letters, numbers & symbols</h2>
                     <div className="my-[25px]">
                         <div className="flex gap-x-2">
@@ -45,7 +45,7 @@ function SingUp(){
                             <input type="checkbox" /> <p>Subscribe to our monthly newsletter</p>
                         </div>
                     </div>
-                    <button className="w-[40%] rounded-md border-[1px] border-black p-4 bg-[#8A33FD] text-white"> Sing up</button>
+                    <button className="w-80 lg:w-40 rounded-md border-[1px] border-black p-4 bg-[#8A33FD] text-white"> Sing up</button>
                     <div className="mb-16 flex">
                     <h1>Already have an  account?</h1><Link to='/profile/*' className="text-[#8A33FD]"> Log in</Link>  
                     </div>
