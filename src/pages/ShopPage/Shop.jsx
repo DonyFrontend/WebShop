fc381ee8cb6c2711d7d44cad94633b1097394424
Benzema9Real <footballfceasports>
2024-05-28 17:55:10 +0600

    adaptive menu

--- src/pages/ShopPage/Shop.jsx
+++ src/pages/ShopPage/Shop.jsx
@@ -25,8 +25,8 @@ const Shop = () => {
     }
     
 
-    return <div className="flex w-[100%] justify-center mt-20">
-        <div className="flex w-[95%] justify-between">
+    return <div className="flex  w-[100%] justify-center mt-20">
+        <div className="flex flex-col md:flex-col lg:flex-row w-[95%] justify-between">
             <div className="lg:w-[15%] p-4 gap-y-3 lg:border-r-[1px] lg:border-r-gray-500">
             <div className='sticky left-0 top-12 hidden lg:flex flex-col gap-y-5 p-3'>
                     <div className='flex justify-between'>
