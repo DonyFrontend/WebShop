575e8e5eef8034f1daaadf2bb5ad31519bd86a43
iskak2007 <isaak9894@gmail.com>
2024-06-13 17:45:04 +0600

    change

--- src/Store/Store.js
+++ src/Store/Store.js
@@ -1,4 +1,4 @@
-import {configureStore, current} from '@reduxjs/toolkit';
+import {configureStore} from '@reduxjs/toolkit';
 import getProductsSlice from '../Slices/getProductsSlice';
 import { SignInTC } from '../Slices/SignInTC';
 import { SignOutUserTC } from '../Slices/SignOutUserTC';
