903c707fe596fa50de244d094299dfb2ebb2425a
Doni <donibro357@gmail.com>
2024-06-14 19:30:39 +0600

    change chats

--- src/pages/AdminPage/AdminPages/Chat.jsx
+++ src/pages/AdminPage/AdminPages/Chat.jsx
@@ -1,24 +1,49 @@
-import {useSelector, useDispatch} from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import { useEffect } from 'react';
 import { getAllUsersTC } from '../../../Slices/getAllUsersSlice';
+import { Box, Button, Card, CardBody, CardHeader, Heading, Text } from '@chakra-ui/react';
 
 const Chat = () => {
-    const {users, isFetch} = useSelector(state => state.getAllUsersSlice);
+    const { users, isFetch } = useSelector(state => state.getAllUsersSlice);
     const dispatch = useDispatch();
-    console.log(users);
 
     useEffect(() => {
         dispatch(getAllUsersTC());
-    })
-    
+    }, [dispatch])
+
     if (isFetch) {
         return <h1>Loading...</h1>
     }
 
+    const findChats = users.filter(item => item.chat[0]);
+    console.log(findChats);
+
+    if (findChats == false) {
+        return <div className='flex gap-x-2'>
+        <span style={{ width: 5, height: 40, backgroundColor: '#8A33FD', borderRadius: 15 }}></span>
+        <h1 className='font-semibold text-3xl'>Apparently, there are no new messages here right now.</h1>
+    </div>
+    }
+
     return <div>
-        {users.map((item, index) => <div key={index}>
-            <p>{item.chat}</p>
-        </div>)}
+        {users.map((item, index) => item.chat[0] === undefined ? '' : <Card key={index} >
+            <CardHeader>
+                <Heading size='md'>{item.name}</Heading>
+            </CardHeader>
+
+            <CardBody>
+                <Box className='flex flex-col gap-y-4'>
+                    {item.chat ? item.chat.map((item, index) => <Text key={index} fontSize='md'>
+                        {item}
+                    </Text>) : ''}
+
+                    <Box className='flex gap-x-3'>
+                        <Button colorScheme='green'>Go to chat</Button>
+                        <Button colorScheme='red'>Delete chat</Button>
+                    </Box>
+                </Box>
+            </CardBody>
+        </Card>)}
     </div>
 }
 
