498544b9a3da19c8d62b7ef12a0006132cfdc470
iskak2007 <isaak9894@gmail.com>
2024-06-22 19:44:48 +0600

    add burger-menu, adaptive profile and try to adaptive wishlist

--- src/pages/ProfilePage/Profile.jsx
+++ src/pages/ProfilePage/Profile.jsx
@@ -5,7 +5,7 @@ import CustomLink from './CustomLink';
 import { Routes, Route } from 'react-router-dom';
 import UserPage from './ProfilePages/User';
 import WishList from './ProfilePages/WishList';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Order from './ProfilePages/Order';
 import ProfileModal from './ProfileModal';
 import { scrollToZero } from '../utils/CustomFC';
@@ -25,11 +25,16 @@ const Profile = () => {
         return <h1 className='font-semibold text-3xl'>Loading...</h1>
     }
 
+    const [click, setClick] = useState(false);
+    function onHandleClick() {
+        setClick(!click)
+    }
+
     return (
         <>
             <div className="flex w-[100%] justify-center mt-32">
                 <div className="w-[90%] flex justify-between">
-                    <div className='w-[20%] flex flex-col gap-y-12 p-5' style={{ borderRight: '1px solid black' }}>
+                    <div className='w-[20%] hidden lg:flex flex-col gap-y-12 p-5' style={{ borderRight: '1px solid black' }}>
                         <div className='hidden lg:flex flex-col gap-y-2'>
                             <div className='flex gap-x-2'>
                                 <span style={{ width: 5, height: 40, backgroundColor: '#8A33FD', borderRadius: 15 }}></span>
@@ -37,14 +42,30 @@ const Profile = () => {
                             </div>
                             <p className='text-gray-400'>Welcome to your Account</p>
                         </div>
-
-                        <div className='flex flex-col gap-y-4 sticky top-24'>
+                         <div className='hidden lg:flex flex-col gap-y-4 sticky top-24'>
                             <CustomLink image={Orders} text={'My orders'} to={'/profile/orders'} />
                             <CustomLink image={Heart} text={'Wishlist'} to={'/profile/wishlist'} />
                             <CustomLink image={User} text={'My info'} to={'/profile/user'} />
                             <ProfileModal />
                         </div>
                     </div>
+
+
+                     {click ? <div className='flex rounded-lg text-xl text-center flex-col fixed top-12 left-0 gap-3 p-5 md:p-12 bg-white border border-black'>
+                            <CustomLink image={Orders} text={'My orders'} to={'/profile/orders'} />
+                            <CustomLink image={Heart} text={'Wishlist'} to={'/profile/wishlist'} />
+                            <CustomLink image={User} text={'My info'} to={'/profile/user'} />
+                            <ProfileModal />
+                            <button className='border border-gray-500 rounded-lg px-5 md:mt-5 md:py-2.5' onClick={onHandleClick}>Close</button>
+                        </div> : ''}
+                            <div className='inline-block lg:hidden'>
+                                <button onClick={onHandleClick}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                                    </svg>
+                                </button>
+                            </div>
+                   
 
                     <div className='w-[75%]'>
                         <Routes>
