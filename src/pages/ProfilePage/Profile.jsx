dc750baaaecdcb3abb746455d9253ef29ae75b4b
Doni <donibro357@gmail.com>
2024-06-22 20:40:16 +0600

    change errors

--- src/pages/ProfilePage/Profile.jsx
+++ src/pages/ProfilePage/Profile.jsx
@@ -68,7 +68,7 @@ const Profile = () => {
                             </div>
                    
 
-                    <div className='w-[75%]'>
+                    <div className='w-[75%] overflow-y-scroll'>
                         <Routes>
                             <Route path='/*' element={<UserPage user={user}/>}></Route>
                             <Route path='wishlist' element={<WishList />}></Route>
